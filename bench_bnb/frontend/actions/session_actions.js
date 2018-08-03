5cbaabad849320f2fa221c0d7c5459ecc2e67b21
richardma <rich.maaa@gmail.com>
2018-08-03 16:37:35 -0700

    Complete reducers

--- bench_bnb/frontend/actions/session_actions.js
+++ bench_bnb/frontend/actions/session_actions.js
@@ -1,15 +1,33 @@
-export const login = (user) => dispatch => (
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+import * as SessionApi from '../util/session_api_util';
 
+export const login = formUser => dispatch => (
+  SessionApi.login(formUser)
+  .then(user => dispatch(receiveCurrentUser(user)))
 );
 
 export const logout = () => dispatch => (
-
+  SessionApi.logout()
+  .then(() => dispatch(logoutCurrentUser()))
 );
 
-export const signup = (user) => dispatch => (
-
+export const signup = (formUser) => dispatch => (
+  SessionApi.signup(formUser)
+  .then(user => dispatch(receiveCurrentUser(user)))
 );
 
-expor receiveCurrentUser = (currentUser) => dispatch => (
+export const receiveCurrentUser = currentUser => dispatch => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
 
-);
+export const logoutCurrentUser = () => dispatch => ({
+  type: LOGOUT_CURRENT_USER
+});
+
+export const receiveErrors = errors => dispatch => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
